abb28be13c253fee9caddaeeca9f73e6a4f085db
Julio Cesar Amador S <t_julioat@soriana.com>
2019-01-10 17:46:16 -0600

    Se incluye spatie

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            'firma:X-Application-Name',
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -38,7 +39,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            'firma:X-Application-Name',
+            'throttle:3,1',
             'bindings',
         ],
     ];
@@ -57,5 +59,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'firma' => \App\Http\Middleware\FirmaMiddleware::class,
     ];
 }
